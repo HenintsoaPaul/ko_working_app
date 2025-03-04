fffde4f0207023f685673f2fbec89a45235fe613
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-04 21:42:53 +0300

    [ui]-Add client form validation in login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,27 @@
-<script setup>
-const formState = reactive({
-    email: '',
-    password: ''
+<script setup lang="ts">
+import { z } from 'zod';
+import type { FormSubmitEvent } from '#ui/types';
+
+const schema = z.object({
+    email: z.string().email('Invalid email'),
+    password: z.string().min(3, 'Must be at least 3 characters')
 });
 
-const onSubmit = () => {
-    console.log("form: ", formState);
+type Schema = z.output<typeof schema>;
+
+const formState = reactive({
+    email: undefined,
+    password: undefined
+});
+
+const onSubmit = (event: FormSubmitEvent<Schema>) => {
+    console.log("form: ", event.data);
 };
 </script>
 
 <template>
     <div>
-        <UForm :state="formState" @submit="onSubmit">
+        <UForm :schema="schema" :state="formState" @submit="onSubmit">
             <UFormGroup label="Email" name="email">
                 <UInput v-model="formState.email" type="email" />
             </UFormGroup>

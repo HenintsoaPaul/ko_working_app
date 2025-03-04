bee1e4bd5d4b8dcfe176e149712751fd28f3bca8
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-04 21:23:37 +0300

    [ui]-Add simple login page

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+const formState = reactive({
+    email: '',
+    password: ''
+});
+
+const onSubmit = () => {
+    console.log("form: ", formState);
+};
+</script>
+
+<template>
+    <div>
+        <UForm :state="formState" @submit="onSubmit">
+            <UFormGroup label="Email" name="email">
+                <UInput v-model="formState.email" type="email" />
+            </UFormGroup>
+
+            <UFormGroup label="Password" name="password">
+                <UInput v-model="formState.password" type="password" />
+            </UFormGroup>
+
+            <UButton type="submit">
+                Submit
+            </UButton>
+        </UForm>
+    </div>
+</template>
+
+<style></style>
